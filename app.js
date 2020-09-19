620390063b065a09910429784f566fd7e2941503
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-19 14:49:40 +0530

    Displaying game in browser

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+
 const Hangman = function(word, guesses){
     this.word = word.toLowerCase().split(''),
     this.guesses = guesses
-    this.guessedLetters = ['c', 'e'];
+    this.guessedLetters = [];
 }
 
 Hangman.prototype.getPuzzle = function(){
@@ -16,8 +17,31 @@ Hangman.prototype.getPuzzle = function(){
     return puzzle;
 }
 
+Hangman.prototype.guessLetter = function(guess){
+    if(this.guesses > 0){
+        guess = guess.toLowerCase();
+        const isUnique = !this.guessedLetters.includes(guess);
+        const isBadGuess = !this.word.includes(guess);
+        if(isUnique){
+            this.guessedLetters.push(guess);
+        }
+        if(isUnique && isBadGuess){
+            this.guesses--;
+        }
+    }
+}
+
 const h1 = new Hangman('CAT', 2);
 const h2 = new Hangman('bhayandar', 5);
 
-console.log(h1.getPuzzle());
-// console.log(h2);
+const puzzleEl = document.querySelector('#puzzle');
+const guessesEl = document.querySelector('#guesses');
+
+puzzleEl.textContent = h1.getPuzzle();
+
+window.addEventListener('keypress', function(e){
+    const guess = e.key.toLowerCase();
+    h1.guessLetter(guess);
+    puzzleEl.textContent = h1.getPuzzle();
+    guessesEl.textContent = h1.guesses;
+});

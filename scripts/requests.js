12fa8a0ddd8f8ad121c1410913691f3772033cc1
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-22 17:42:03 +0530

    Updating Fetch URL

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,7 +1,7 @@
 // HTTP Request
 
 const getPuzzleAsync = async (noOfWords) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${noOfWords}`);
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${noOfWords}`);
     if(response.status === 200) {
         const data = await response.json();
         return data.puzzle;
